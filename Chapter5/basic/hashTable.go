17d898a5efd6e1d15f4bd3220eca62090a00b25b
fengzw <zhiwei.feng1995@gmail.com>
2021-01-09 00:00:25 +0800

    hash table

--- Chapter5/basic/hashTable.go
+++ Chapter5/basic/hashTable.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 const SIZE = 15
 
 type Node struct {
@@ -14,4 +16,59 @@ type HashTable struct {
 
 func hashFunction(i, size int) int {
 	return (i % size)
+}
+
+// 没有检查重复的value
+func insert(hash *HashTable, value int) int {
+	index := hashFunction(value, hash.Size)
+	element := Node{Value: value, Next: hash.Table[index]}
+	hash.Table[index] = &element
+	return index
+}
+
+func traverse(hash *HashTable) {
+	for k := range hash.Table {
+		if hash.Table[k] != nil {
+			t := hash.Table[k]
+			for t != nil {
+				fmt.Printf("%d -> ", t.Value)
+				t = t.Next
+			}
+			fmt.Println()
+		}
+	}
+}
+
+func lookup(table *HashTable, value int) bool {
+	index := hashFunction(value, table.Size)
+	if table.Table[index] != nil {
+		t := table.Table[index]
+		for t != nil {
+			if t.Value == value {
+				return true
+			}
+			t = t.Next
+		}
+	}
+	return false
+}
+
+func main() {
+	table := make(map[int]*Node, SIZE)
+	hash := &HashTable{
+		Table: table,
+		Size:  SIZE,
+	}
+	fmt.Println("Number of spaces:", hash.Size)
+	for i := 0; i < 120; i++ {
+		insert(hash, i)
+	}
+	traverse(hash)
+
+	fmt.Println("****lookup part****")
+	for i := 120; i < 125; i++ {
+		if !lookup(hash, i) {
+			fmt.Printf("%d is not in the hash table!\n", i)
+		}
+	}
 }
