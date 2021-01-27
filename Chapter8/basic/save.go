f0d6fb68c06f908651bbfbf4e8e875a092bbb51e
fengzw <zhiwei.feng1995@gmail.com>
2021-01-27 21:51:56 +0800

    1. saving data to file 2. Loading and saving data on disk

---
+++ Chapter8/basic/save.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	s := []byte("Data to write\n")
+
+	// 1. fmt.Fprintf写入
+	f1, err := os.Create("f1.txt")
+	if err != nil {
+		fmt.Println("Cannot create file", err)
+		return
+	}
+	defer f1.Close()
+	fmt.Fprintf(f1, string(s))
+
+	// 2. *File.WriteString写入
+	f2, err := os.Create("f2.txt")
+	if err != nil {
+		fmt.Println("Cannot create file", err)
+		return
+	}
+	defer f2.Close()
+	n, err := f2.WriteString(string(s))
+	fmt.Printf("wrote %d bytes\n", n)
+
+	// 3. bufio Writer.WriteString
+	f3, err := os.Create("f3.txt")
+	if err != nil {
+		fmt.Println("Cannot create file", err)
+		return
+	}
+	defer f3.Close()
+	w := bufio.NewWriter(f3)
+	n, err = w.WriteString(string(s))
+	fmt.Printf("wrote %d bytes\n", n)
+	w.Flush()
+
+	// 4. ioutil.WriteFile
+	f4 := "f4.txt"
+	err = ioutil.WriteFile(f4, s, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 5. io.WriteString
+	f5, err := os.Create("f5.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f5.Close()
+	n, err = io.WriteString(f5, string(s))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("wrote %d bytes\n", n)
+}
