07a8eb6e6b729df0a28085d7e64208bf2940907a
fengzw <zhiwei.feng1995@gmail.com>
2021-01-28 17:39:31 +0800

    1. unix file permissions 2. handle signals

---
+++ Chapter8/practice/handleAll.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func handle(signal os.Signal) {
+	fmt.Println("Received:", signal)
+}
+
+func main() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs)
+
+	go func() {
+		for {
+			sig := <-sigs
+			switch sig {
+			case os.Interrupt:
+				handle(sig)
+			case syscall.SIGTERM:
+				handle(sig)
+				os.Exit(0)
+			default:
+				fmt.Println("Ignoring:", sig)
+			}
+		}
+	}()
+
+	for {
+		fmt.Printf(".")
+		time.Sleep(30 * time.Second)
+	}
+}
